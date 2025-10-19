08b0a47f9f8b5823b32881206bc8f87122d5a127
benjamin10ks <bjkmanning@gmail.com>
2025-10-19 13:31:14 -0600

    user add/delete from database

--- internal/services/user.go
+++ internal/services/user.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/benjamin10ks/goCloudStorage/internal/models"
 	"golang.org/x/crypto/bcrypt"
@@ -26,7 +27,7 @@ func (u *UserService) CreateUser(username, password string) (*models.User, error
 	}
 	query := `INSERT INTO users (user_name, password_hash) VALUES ($1, $2) RETURNING user_id`
 	userID := 0
-	u.db.QueryRow(query, username, passwordHash).Scan(&userID)
+	err = u.db.QueryRow(query, username, passwordHash).Scan(&userID)
 	if err != nil {
 		return nil, err
 	}
@@ -38,8 +39,18 @@ func (u *UserService) CreateUser(username, password string) (*models.User, error
 }
 
 func (u *UserService) DeleteUser(userID int) error {
-	// Placeholder implementation
-	query := `DELETE FROM users WHERE user_id = $1`
+	query := `DELETE FROM users WHERE user_id = $2`
+	res, err := u.db.Exec(query, userID)
+	if err != nil {
+		return err
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if rows != 1 {
+		log.Fatalf("expected to affect 1 row, affected %d", rows)
+	}
 	return nil
 }
 
