93e63aaaf9ff95181ec6b6412a22c45d84df124e
benjamin10ks <bjkmanning@gmail.com>
2026-03-18 21:34:13 -0600

    auth constructor

--- service_auth.go
+++ service_auth.go
@@ -4,12 +4,25 @@ import (
 	"crypto/rand"
 	"database/sql"
 	"encoding/hex"
+
+	"github.com/go-webauthn/webauthn/webauthn"
 )
 
-type AuthService struct{}
+type AuthService struct {
+	webauthn *webauthn.WebAuthn
+	db       *sql.DB
+}
 
-func NewAuthService() *AuthService {
-	return &AuthService{}
+func NewAuthService(db *sql.DB) (*AuthService, error) {
+	wauthn, err := webauthn.New(&webauthn.Config{
+		RPDisplayName: "My Cloud Storage",
+		RPID:          "localhost",                       // Adjust when deploying
+		RPOrigins:     []string{"http://localhost:8080"}, // Adjust when deploying
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &AuthService{webauthn: wauthn, db: db}, nil
 }
 
 func (a *AuthService) Register(token string, db *sql.DB) (User, error) {
